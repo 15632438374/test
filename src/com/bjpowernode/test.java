fa4618f56521073d6ef1bd5c69d3141d0971b613
15632438374 <2037862068@qq.com>
2020-12-01 16:13:54 +0800

    test立项

---
+++ src/com/bjpowernode/test.java
@@ -0,0 +1,15 @@
+package com.bjpowernode;
+
+/**
+ * @ClassName test
+ * @Description test
+ * @Author PowerNode
+ * @Date 2020/12/1 15:59
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        int a=1;
+        int b=a;
+    }
+}
